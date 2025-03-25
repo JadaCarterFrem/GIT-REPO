f853c981e1491d46f2362d097d1b52fb7b1a5e46
Jada Carter-Frem <carterfrem2@gmail.com>
2025-03-25 17:38:39 -0400

    update code

--- assignment6/script.js
+++ assignment6/script.js
@@ -1,12 +1,13 @@
-// Display a message in the browser console
-console.log("CSS + JavaScript is powerful!");
 
-// Show an alert message when the page loads
-window.onload = function () {
-    alert("Welcome to Assignment 5 with JavaScript!");
-};
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("CSS + JavaScript is powerful!");
 
-// Function to toggle between light mode and dark mode
+    if (!sessionStorage.getItem("alertShown")) {
+        alert("Welcome to Assignment 5 with JavaScript!");
+        sessionStorage.setItem("alertShown", "true");
+    }
+});
+
 function changeTheme() {
     document.body.classList.toggle("dark-mode");
 }
